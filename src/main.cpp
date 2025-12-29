bbc83dd9eeff5cd3b0476b84d72372ccbd9dd43c
devyanshnandeshwar <devyansh.cloud@gmail.com>
2025-12-29 23:14:24 +0530

    feat: add replay loop for multiple game rounds

--- src/main.cpp
+++ src/main.cpp
@@ -6,42 +6,63 @@ using namespace std;
 
 int main()
 {
+    srand(time(0)); // Seed one
+
     int userChoice;
-
-    cout << "=== Rock Paper Scissors ===" << endl;
-    cout << "Enter your choice: ";
-    cout << "0 - Rock" << endl;
-    cout << "1 - Paper" << endl;
-    cout << "2 - Scissors" << endl;
-    cout << "Choices: ";
-    cin >> userChoice;
-
-    // Seed randon number generator
-    srand(time(0));
-    int computerChoice = rand() % 3;
+    char playAgain = 'y';
 
     string choices[] = {"Rock", "Paper", "Scissors"};
 
-    cout << "\nYou chose: " << choices[userChoice] << endl;
-    cout << "Computer chose: " << choices[computerChoice] << endl;
-
-    // Decide winner
-
-    if (userChoice == computerChoice)
+    while (playAgain == 'y' || playAgain == 'Y')
     {
-        cout << "Result: Draw!" << endl;
-    }
-    else if (
-        (userChoice == 0 && computerChoice == 2) ||
-        (userChoice == 1 && computerChoice == 0) ||
-        (userChoice == 2 && computerChoice == 1))
-    {
-        cout << "Result: You Win!" << endl;
-    }
-    else
-    {
-        cout << "Result: Computer Wins!" << endl;
-    }
+        cout << "=== Rock Paper Scissors ===" << endl;
+        cout << "0 - Rock" << endl;
+        cout << "1 - Paper" << endl;
+        cout << "2 - Scissors" << endl;
 
+        // Input validation
+        while (true)
+        {
+            cout << "Enter your choice: ";
+            cin >> userChoice;
+
+            if (cin.fail() || userChoice < 0 || userChoice > 2)
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << "Invalid input. Please enter 0, 1, or 2." << endl;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        int computerChoice = rand() % 3;
+
+        cout << "\nYou chose: " << choices[userChoice] << endl;
+        cout << "Computer chose: " << choices[computerChoice] << endl;
+
+        // Decide winner
+        if (userChoice == computerChoice)
+        {
+            cout << "Result: Draw!" << endl;
+        }
+        else if (
+            (userChoice == 0 && computerChoice == 2) ||
+            (userChoice == 1 && computerChoice == 0) ||
+            (userChoice == 2 && computerChoice == 1))
+        {
+            cout << "Result: You Win!" << endl;
+        }
+        else
+        {
+            cout << "Result: Computer Wins!" << endl;
+        }
+
+        cout << "\nPlay again (y/n): ";
+        cin >> playAgain;
+    }
+    cout << "\nThanks for playing!" << endl;
     return 0;
 }
